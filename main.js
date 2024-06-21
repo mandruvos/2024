5789e1d4058e07a449b10f5200ff38ff3723ed83
ghost <117953431+mandruvos@users.noreply.github.com>
2024-06-21 08:32:59 -0300

    Add files via upload
    Signed-off-by: ghost. <117953431+mandruvos@users.noreply.github.com>

--- main.js
+++ main.js
@@ -23,5 +23,7 @@ const botoes = document.querySelectorAll('.botao');
 
  const contadores = document.querySelectorAll('.contador');
  contadores [0].textContent = 'Contagem regressiva';
- const tempoObjetivo1 = new Date('2024-06-21');
- contadores[0].textContent = tempoObjetivo1
+ const tempoObjetivo1 = new Date('2024-10-05T00:00:00');
+ let tempoAtual = new Date ();
+
+ contadores[0].textContent = tempoObjetivo1 - tempoAtual;
